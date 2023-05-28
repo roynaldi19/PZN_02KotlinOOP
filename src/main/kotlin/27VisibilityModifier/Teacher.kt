5d9cc42bbc9e71f471dbf784b87444031b1d6104
roynaldi19 <roynaldi19@gmail.com>
2023-05-28 17:45:23 +0700

    update 23

--- src/main/kotlin/27VisibilityModifier/Teacher.kt
+++ src/main/kotlin/27VisibilityModifier/Teacher.kt
@@ -5,3 +5,16 @@ class Teacher(val name: String) {
         println("Teach!")
     }
 }
+
+/**
+ * public -> deafault bisa diaksesk dari mana saja
+ * private -> diaakses dari tempat deklarasinya
+ * protected -> diakses dari tempat deklasi dan turunannnya
+ * internal -> di akses dari project yang sama
+ */
+
+fun main() {
+    val teacher = Teacher("Roy")
+    println(teacher.name)
+    teacher.teach()
+}
